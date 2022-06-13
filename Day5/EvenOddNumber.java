7b2e26d855a709897188cacc6be7fbb5dfaa0b3b
sushant <sgharge299@gmail.com>
2022-06-13 16:59:51 +0530

    [sushant]added the programe of Number is Even or Odd

---
+++ Day5/EvenOddNumber.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class EvenOddNumber {
+	public static void main (String[] args) {
+	     
+	     System.out.println("Enter a number: ");
+	     Scanner reader = new Scanner(System.in);
+	     int num = reader.nextInt();
+
+	     if (num % 2 == 0)
+
+	     System.out.println(num + " is Even Number ");
+	     
+	     else
+
+	     System.out.println(num + " is odd Number ");
+
+	}
+}
