c3a3d99e20e3d46d6bb22f0d93a6967d914486c0
sushant <sgharge299@gmail.com>
2022-06-13 16:30:41 +0530

    [sushant]worked on the programe of Quotient and Remainder

---
+++ Day5/QuotientRemainder.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class QuotientRemainder {
+	public static void main (String[] args) {
+	     
+	     System.out.println("Enter first number: ");
+	     Scanner sc = new Scanner(System.in);
+	     int num1 = sc.nextInt();
+
+	     System.out.println("Enter second number: ");
+	     int num2 = sc.nextInt();
+ 		
+	     int quotient = 0, remainder = 0;
+
+	     quotient = (num1 / num2 ) - remainder;
+	     remainder = num1 - num2 * quotient;
+
+	     System.out.println("Quotient = " + quotient);
+	     System.out.println("Remainder = " + remainder);
+
+	}
+}
