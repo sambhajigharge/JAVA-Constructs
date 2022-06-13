fc6d5d9fc553d3fb8d9316f0854b85bba1b4688b
sushant <sgharge299@gmail.com>
2022-06-13 14:24:05 +0530

    [sushant]worked on the leap year program

---
+++ Day5/leapYear.java
@@ -0,0 +1,18 @@
+ 
+
+import java.util.Scanner;
+public class leapYear {
+    public static void main (String[] args) {
+       int year;
+	System.out.println("Enetr a year : ");
+	Scanner sc = new Scanner(System.in);
+	year = sc.nextInt();
+
+	if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+
+	    System.out.println("Given year is a leap year");
+	else
+	    System.out.println("Given year is not a leap year");
+
+      }
+}
