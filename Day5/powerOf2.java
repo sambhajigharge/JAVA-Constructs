b1bafe085976e805c67d2c65b3d7528e5c5e05ab
sushant <sgharge299@gmail.com>
2022-06-13 14:57:08 +0530

    [sushant]worked on the power of 2 program

---
+++ Day5/powerOf2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class powerOf2 {
+
+	public static void main (String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter number : ");
+
+		int n = sc.nextInt();
+
+		for (int i=0 ; i<=n ; i++) {
+
+		System.out.println(i + " " + ((int)(Math.pow(2,i))));
+
+		}
+
+	}
+
+}
