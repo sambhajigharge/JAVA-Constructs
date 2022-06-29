a727cceae46a95e414d7bef36df8c35c83f02dd9
sushant <sgharge299@gmail.com>
2022-06-29 16:38:23 +0530

    adding day5 folder

--- Day3/EmpWageBuilder.java
+++
@@ -1,7 +0,0 @@
-package JAVA.Day3;
-
-public class EmpWageBuilder {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation Program");
-    }
-}
