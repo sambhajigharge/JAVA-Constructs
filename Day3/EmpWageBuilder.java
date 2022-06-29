f3e037faa7e9c4a44b1ea050067924ce8f053682
sushant <sgharge299@gmail.com>
2022-06-29 13:00:34 +0530

    [sushant]Started the programe with Welcome message.

---
+++ Day3/EmpWageBuilder.java
@@ -0,0 +1,7 @@
+package JAVA.Day3;
+
+public class EmpWageBuilder {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program");
+    }
+}
